fa8f33eb22931ca0409afd4645ea6e494bb15796
Pablo M. Cordero <corderopablomartin06@gmail.com>
2024-09-16 07:37:23 +0000

    cambio de estilos

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => (
+	<footer className="footer mt-auto py-3 text-center">
+		<a href="https://github.com/Pablocirus89" className="ms-2" target="_blank" rel="noopener noreferrer">
+			<i className="fa-brands fa-github fa-2x"></i>			
+		</a>
+		<p className="mt-2">
+			<a href="https://github.com/Pablocirus89" target="_blank" rel="noopener noreferrer">
+				https://github.com/Pablocirus89
+			</a>
+		</p>
+	</footer>
+);
+
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import Card from "./card"
 import SecondCounter from "./secondCounter";
+import Footer from "./footer";
 
 
 const Home = ({number, number2, number3, number4, number5, number6}) => {
 	return (
 		<div className="text-center pt-5">
 			<SecondCounter number={number} number2={number2} number3={number3} number4={number4} number5={number5} number6={number6}/>
+			<Footer />
 		</div>
+		
 	);
 };
 
