28137c6b81848026f7539b44e50dcb995b388c5d
Pablo M. Cordero <corderopablomartin06@gmail.com>
2024-09-19 00:34:29 +0000

    Footer push end

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<a href="https://github.com/Pablocirus89" className="ms-2" target="_blank" rel="noopener noreferrer">
-			<i className="fa-brands fa-github fa-2x"></i>			
+			<i className="fa-brands fa-github fa-2x"></i>
 		</a>
 		<p className="mt-2">
 			<a href="https://github.com/Pablocirus89" target="_blank" rel="noopener noreferrer">
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import SecondCounter from "./secondCounter";
 import Footer from "./footer";
 
-
-const Home = ({number, number2, number3, number4, number5, number6}) => {
+const Home = ({ number, number2, number3, number4, number5, number6 }) => {
 	return (
-		<div className="text-center pt-5">
-			<SecondCounter number={number} number2={number2} number3={number3} number4={number4} number5={number5} number6={number6}/>
+		<div className="d-flex flex-column min-vh-100">
+			<div className="text-center pt-5 flex-grow-1">
+				<SecondCounter
+					number={number}
+					number2={number2}
+					number3={number3}
+					number4={number4}
+					number5={number5}
+					number6={number6}
+				/>
+			</div>
 			<Footer />
 		</div>
-		
 	);
 };
 
 export default Home;
+
